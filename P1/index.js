b449c4b08af229ca92e34aa18cdb1cf0fa804ded
Hauk Olaussen <olaussen@users.noreply.github.com>
2020-08-20 16:50:41 +0200

    Added frames and resized canvas pixels to be not retarded

--- P1/index.js
+++ P1/index.js
@@ -1,5 +1,44 @@
 
+const canvas = document.getElementById("canvas-art")
+const ctx = canvas.getContext("2d")
+
+function matchCanvasPixelsToDisplayPixels() {
+   const width = canvas.clientWidth;
+   const height = canvas.clientHeight;
+
+   if (canvas.width !== width || canvas.height !== height) {
+     canvas.width = width;
+     canvas.height = height;
+     return true;
+   }
+
+   return false;
+}
+
+
+function initiateCanvasArt() {
+    matchCanvasPixelsToDisplayPixels()
+
+    const w = canvas.width
+    const h = canvas.height
+    const offset = w/10
+
+    ctx.fillstyle = "gold"
+    ctx.fillRect(0, 0, w, h);
+    ctx.clearRect(offset, offset, w-2*offset, h-2*offset);
+    ctx.strokeRect(offset+0.25*offset, offset+0.25*offset, w-2.5*offset, h-2.5*offset);
+}
 
 $("document").ready(() => {
-    console.log("Testing console log via jQuery")
+    initiateCanvasArt()
+    const $canvas = $("#canvas-art")
+    const $svg = $("#svg-art")
+
+    $canvas.click(() => {
+        console.log("clicked canvas")
+    })
+
+    $svg.click(() => {
+        console.log("clicked svg")
+    })
 })
